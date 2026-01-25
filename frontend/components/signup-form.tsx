c8a62819b96eed295d95dd4f37c8d0b4973a4200
Miron <mironchik515ke@gmail.com>
2026-01-25 21:36:17 +0300

    feat(sign-up): validation on sign-up form with react-hook-form/zod

--- frontend/components/signup-form.tsx
+++ frontend/components/signup-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,11 +15,41 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z
+  .object({
+    fullname: z.string().trim().min(3),
+    email: z.string().email(),
+    password: z.string().trim().min(8),
+    confirmPassword: z.string().trim().min(8),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords dont match",
+    path: ["confirmPassword"],
+  });
+
+type SignUpFormFields = z.infer<typeof schema>;
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormFields>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<SignUpFormFields> = async (data) => {
+    await new Promise((res) => setTimeout(res, 1000));
+    // взаимодействие с бэком
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -29,15 +60,27 @@ export function SignupForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="name">Full Name</FieldLabel>
-                <Input id="name" type="text" placeholder="John Doe" required />
+                <Input
+                  {...register("fullname")}
+                  id="name"
+                  type="text"
+                  placeholder="John Doe"
+                  required
+                />
+                {errors.fullname && (
+                  <p className="text-xs text-red-500">
+                    {errors.fullname.message}
+                  </p>
+                )}
               </Field>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
+                  {...register("email")}
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -48,22 +91,41 @@ export function SignupForm({
                 <Field className="grid grid-cols-2 gap-4">
                   <Field>
                     <FieldLabel htmlFor="password">Password</FieldLabel>
-                    <Input id="password" type="password" required />
+                    <Input
+                      {...register("password")}
+                      id="password"
+                      type="password"
+                      required
+                    />
                   </Field>
                   <Field>
                     <FieldLabel htmlFor="confirm-password">
                       Confirm Password
                     </FieldLabel>
-                    <Input id="confirm-password" type="password" required />
+                    <Input
+                      {...register("confirmPassword")}
+                      id="confirm-password"
+                      type="password"
+                      required
+                    />
                   </Field>
                 </Field>
                 <FieldDescription>
                   Must be at least 8 characters long.
                 </FieldDescription>
+                {errors.confirmPassword && (
+                  <p className="text-xs text-red-500">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
               </Field>
               <Field>
-                <Button type="submit" className="bg-primary">
-                  Create Account
+                <Button
+                  type="submit"
+                  className="bg-primary"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Loading..." : "Create Account"}
                 </Button>
                 <FieldDescription className="text-center">
                   Already have an account? <a href="#">Sign in</a>
